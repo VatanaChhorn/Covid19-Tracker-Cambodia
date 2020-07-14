66feeffdc8c0983efd76afae47e7587a199ce465
VatanaChhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-07-14 19:08:44 +0700

    Finished UI Design
    Finished all UI - Front End Design.
    
    Waiting the Back-End Design to finish the project.

---
+++ Cambodia Covid19 Tracker/C/SecondScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondScreenViewController.swift
+//  Cambodia Covid19 Tracker
+//
+//  Created by Chhorn Vatana on 7/14/20.
+//  Copyright © 2020 Chhorn Vatana. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cambodia Covid19 Tracker/C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cambodia Covid19 Tracker
+//
+//  Created by Chhorn Vatana on 7/9/20.
+//  Copyright © 2020 Chhorn Vatana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
