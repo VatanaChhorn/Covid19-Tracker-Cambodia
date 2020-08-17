becf55509eb76aac0193becfcfd39db1fbfbf1c0
VatanaChhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-08-17 12:06:02 +0700

    Push notification every day at 8AM.

--- Cambodia Covid19 Tracker/SceneDelegate.swift
+++ Cambodia Covid19 Tracker/SceneDelegate.swift
@@ -21,33 +21,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        print("sceneDidEnterBackground")
+                let content = UNMutableNotificationContent()
+                content.sound = UNNotificationSound.default
+                content.title = "Covid19 Tracker Cambodia"
+                content.subtitle = "Bring your mask every time you're going out!"
+        content.body = "The last time you checked was \(UserDefaults.standard.integer(forKey: "NewCasesData")) cases in total.\nDon't be panic, strictly follow recommendations of the local government and stay safe. ✨"
+        
+                var dateComp = DateComponents()
+                dateComp.hour = 8
+                dateComp.minute = 0
+        
+                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: true)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    if let error = error {
+                        print("Push local notification error: \(error)")
+                    }
     }
 
 
 }
 
+}
