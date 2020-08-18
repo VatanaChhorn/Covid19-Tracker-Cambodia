f95d821ed23edfd9ca35e373946d79b4e8480a1d
VatanaChhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-08-18 18:11:02 +0700

    Add delay for notification - AsyncAfter

--- Cambodia Covid19 Tracker/C/MainController.swift
+++ Cambodia Covid19 Tracker/C/MainController.swift
@@ -16,24 +16,24 @@ class MainController: UIViewController {
     let source = Sources()
     let defaults = UserDefaults.standard
     
+    override func viewWillAppear(_ animated: Bool) {
+           coronaVirusManager.delegate = self
+           coronaVirusManager.getTheDataFromAPI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //Declare delegate and call the functions
-        coronaVirusManager.delegate = self
-        coronaVirusManager.getTheDataFromAPI()
-        DispatchQueue.main.async {
-            print(self.defaults.bool(forKey: Sources.Userdefualts.checkInternetConnection))
-            if self.defaults.bool(forKey: Sources.Userdefualts.checkInternetConnection) == true {
-                let alert = UIAlertController(title: "Connection Failed 🤯", message: "The Internet connection appears to be offline.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Understood", style: .default, handler:  { (action) in
-                    self.view.alpha = 1
-                }))
-                self.view.alpha = 0.2
-                self.present(alert, animated: true, completion: nil)
-                print("Triggered")
-            }
-            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4.0)  {
+                  if self.defaults.bool(forKey: Sources.Userdefualts.checkInternetConnection) == true {
+                      let alert = UIAlertController(title: "Connection Failed 🤯", message: "The Internet connection appears to be offline.", preferredStyle: .alert)
+                      alert.addAction(UIAlertAction(title: "Understood", style: .default, handler:  { (action) in
+                          self.view.alpha = 1
+                      }))
+                      self.view.alpha = 0.2
+                      self.present(alert, animated: true, completion: nil)
+                      print("Triggered")
+                  }
         }
         
         //Display the defualtData to the Screen
--- Cambodia Covid19 Tracker/SceneDelegate.swift
+++ Cambodia Covid19 Tracker/SceneDelegate.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     let defaults = UserDefaults.standard
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         //Set internet connection to false
         defaults.set(false, forKey: Sources.Userdefualts.checkInternetConnection)
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
@@ -34,36 +34,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             }
         }
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
-       
-        
         print("sceneDidEnterBackground")
-                let content = UNMutableNotificationContent()
-                content.sound = UNNotificationSound.default
-                content.title = "Covid19 Tracker Cambodia"
-                content.subtitle = "Bring your mask every time you're going out!"
+        let content = UNMutableNotificationContent()
+        content.sound = UNNotificationSound.default
+        content.title = "Covid19 Tracker Cambodia"
+        content.subtitle = "Bring your mask every time you're going out!"
         content.body = "The last time you checked was \(defaults.integer(forKey: Sources.Userdefualts.newCasesData)) cases in total.\nDon't be panic, strictly follow recommendations of the local government and stay safe. ✨"
         
-                var dateComp = DateComponents()
-                dateComp.hour = 8
-                dateComp.minute = 0
+        var dateComp = DateComponents()
+        dateComp.hour = 8
+        dateComp.minute = 0
         
-                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: true)
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                UNUserNotificationCenter.current().add(request) { (error) in
-                    if let error = error {
-                        print("Push local notification error: \(error)")
-                    }
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Push local notification error: \(error)")
+            }
+        }
+        
+        
     }
-
-       
-}
-
+    
 }
